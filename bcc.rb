a6a80f799de23a3ea0baaf082163015c866a9983
Cherie Lampley <cherielampley@Cheries-MacBook-Pro.local>
2022-09-27 19:50:55 -0400

    updated bingo card

--- bcc.rb
+++ bcc.rb
@@ -6,7 +6,7 @@ bingo_card = <<-BINGO
 +---+---+---+---+---+
 |   |   |   |   |   |
 +---+---+---+---+---+
-|   |   |   |   |   |
+|   |   | F  |   |   |
 +---+---+---+---+---+
 |   |   |   |   |   |
 +---+---+---+---+---+
@@ -15,3 +15,15 @@ bingo_card = <<-BINGO
 BINGO
 
 puts bingo_card
+
+b = (1..15).to_a.sample(5)
+i = (16..30).to_a.sample(5)
+n = (31..45).to_a.sample(4)
+g = (46..60).to_a.sample(5)
+o = (61..75).to_a.sample(5)
+
+puts b_col.inspect
+puts i_col.inspect
+puts n_col.inspect
+puts g_col.inspect
+puts o_col.inspect
